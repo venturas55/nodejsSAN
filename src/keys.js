a13852b1a41667048c7a80d5cb78b0d95590c2cb
Adrian De Haro Ortega <venturas55@hotmail.com>
2022-08-19 12:04:35 +0200

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,11 +1,8 @@
 module.exports ={
     database:{
-        //host:'152.228.133.198',
         host:'localhost',
-       // user:'venturas2',
-       // password:'Valen&cia1',
         user:'venturas',
-        password:'weR65hS',
+        password:'xxx',
         database:'san2',
         masterPass:'2a$10$.6weXYERL6XMB7nb0xcMTus/Qbi4aotItCWLs3QyBDk7cup4oCOo.'
         }
